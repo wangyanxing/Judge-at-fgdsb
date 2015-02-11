6923c0aabff275eb105eabf1808f6b5c066623b1
Yanxing Wang <demiwangya@gmail.com>
2015-02-11 00:30:56 -0800

    a-plus-b updated

--- app/script/init.js
+++ app/script/init.js
@@ -15,17 +15,21 @@ var splashwin = gui.Window.open('splash/loading.html', {
 var pkg = require('../package.json');
 var upd = new updater(pkg);
 
+//alert(upd.getAppPath());
+//alert(upd.getAppExec());
+//alert(cur_dir);
+
 var update = function() {
     upd.download(function(error, filename) {
         if (!error) {
             console.log(filename);
+            //gui.App.quit();
         } else {
             console.log(error);
         }
     }, function (state) {
         console.log('percent', state.percent);
     });
-
 };
 
 upd.checkNewVersion(function(error, newVersionExists, manifest) {
@@ -34,10 +38,21 @@ upd.checkNewVersion(function(error, newVersionExists, manifest) {
         return;
     }
     if (!newVersionExists) {
-        var msg = "<table style=\"width:100%; font-size: 15px;\"><tr><td>Current version:</td><td>" +
-            pkg.version + "</td> </tr><tr><td>New version:</td><td>" +
-            manifest.version + "</td> </tr><tr><td>Update log:</td><td>" +
-            manifest.update_log + "</td> </tr></table>";
+        var msg = '<div class="callout callout-info">\
+                <h4>New version available!</h4>\
+                <table style=\"width:100%; font-size: 15px;\"><tr><td>Current version:</td><td>' +
+            pkg.version + '</td> </tr><tr><td>New version:</td><td>' +
+            manifest.version + '</td> </tr><tr><td>Update log:</td><td>' +
+            manifest.update_log + '</td> </tr></table>\
+            </div>\
+            <div id="download-panel" style="display: none;">\
+                <br>\
+                <div class="progress sm progress-striped active"> \
+                  <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%"> \
+                  <span class="sr-only">20% Complete</span> \
+                  </div> \
+                </div>\
+            </div>';
 
         bootbox.dialog({
             message: msg,
@@ -52,7 +67,9 @@ upd.checkNewVersion(function(error, newVersionExists, manifest) {
                     label: "Update",
                     className: "btn-success",
                     callback: function() {
+                        $('#download-panel').show("slow");
                         update();
+                        return false;
                     }
                 }
             }
@@ -86,6 +103,7 @@ try {
 
 win.show();
 //win.maximize();
+win.focus();
 
 $(window).on('dragover', function (e) {
     e.preventDefault();
