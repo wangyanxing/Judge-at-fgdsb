ffe7fbbe541a1425eb7d211f6c8eb5e3bfa48769
Yanxing Wang <demiwangya@gmail.com>
2015-02-16 14:44:34 -0800

    recover-count-array updated

--- judge/build_tests/recover-count-array.rb
+++ judge/build_tests/recover-count-array.rb
@@ -6,11 +6,41 @@ class Test_recover_count_array < TestBase
     super(name)
   end
 
-  def add_test
+  def gen(n)
+    arr, cnt = (1..n).to_a.shuffle, []
+    arr.each_with_index do |num,i|
+      count = 0
+      ((i+1)...n).each do |j|
+        count += 1 if arr[j] < num
+      end
+      cnt << count
+    end
+    [arr, cnt]
+  end
+
+  def add_test(n)
+    org, cnt = gen(n)
+    @test_in[0] << cnt
+    @test_out << org
   end
 
   def gen_tests
     @test_in, @test_out = [[]], []
+
+    @test_in[0] << [3, 0, 1, 0]
+    @test_out << [4, 1, 3, 2]
+
+    30.times do
+      add_test rand(3...50)
+    end
+
+    30.times do
+      add_test rand(30...50)
+    end
+
+    39.times do
+      add_test rand(300...500)
+    end
   end
 end
 
---
+++ judge/python/tests/recover_count_array.py
@@ -0,0 +1,44 @@
+from common import *
+from solution import *
+import copy
+import sys
+import datetime
+
+num_test = 100
+true, false = True, False
+in_0 = []
+in_org_0 = []
+out = []
+
+
+def load_test():
+    f = open('judge/tests/recover-count-array.txt', 'r')
+    global in_0, in_org_0
+    in_0 = read_int_matrix(f)
+    in_org_0 = copy.deepcopy(in_0)
+    global out
+    out = read_int_matrix(f)
+    f.close
+
+def judge():
+    load_test()
+    capture_stdout()
+    start_time = datetime.datetime.now()
+    for i in range(num_test):
+        print ('Testing case #' + str(i+1))
+        answer = recover(in_0[i]) 
+        if (answer != out[i]):
+            release_stdout()
+            out_str = str(i+1) + " / " + str(num_test) + ";"
+            out_str += str(in_org_0[i])
+            out_str += ";"
+            out_str += str(answer)
+            out_str += ";"
+            out_str += str(out[i])
+            print(out_str)
+            return
+
+    release_stdout()
+    delta = datetime.datetime.now() - start_time
+    runtime = str(int(delta.total_seconds() * 1000))
+    print('Accepted;' + runtime)
