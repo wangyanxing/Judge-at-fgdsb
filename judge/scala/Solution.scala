5af05c1737b74c8fc6e7784d1d7994762ce13838
Yanxing Wang <demiwangya@gmail.com>
2015-02-24 19:51:34 -0800

    wiggle-sort updated

--- judge/scala/Solution.scala
+++ judge/scala/Solution.scala
@@ -1,21 +1,5 @@
 package judge; object Solution {
-	def wiggle_sort(arr: Array[Int]): Array[Int] = {
-	    val n = arr.length;
-        if(n == 0) return Array();
-        var s = arr.clone();
-        
-        var flag = true;
-        var current = s(0);
-        for (i <- 0 until n-1) {
-            if ((flag && current > s(i+1)) || (!flag && current < s(i+1))) {
-                s(i) = s(i+1);
-            } else {
-                s(i) = current;
-                current = s(i+1);
-            }
-            flag = !flag;
-        }
-        s(n-1) = current;
-        s;
+	def wiggle_sort(arr: List[Int]): List[Int] = {
+	    List[Int](1,2,3);
 	}
 }
